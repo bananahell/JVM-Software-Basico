983f513f4635760fa2f8d43c2005cc3ee47d26f6
mariana <marisampa26@gmail.com>
2021-09-05 16:09:30 -0300

    update _main

--- fonte/src/_main.cpp
+++ fonte/src/_main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <string>
+#include "Classfile.h"
+/*#include "leitor.h"*/
 
-#include "fodase.h"
 
-int main() {
-  fodase();
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  Classfile *classfile;
+  char *Arquivo;
+
+if(argc > 1) {
+    Arquivo = (char *) malloc(strlen(argv[1]) * sizeof(char));
+    strcpy(Arquivo, argv[1]);
+  } else {
+    printf("O arquivo .class nao foi incluido.\nEncerrando o programa.......\n");
+    exit(0);
+  }
+
+  class_file = (Classfile *) malloc(sizeof(Classfile));
+
+  class_file = lerArquivo(class_file, nomeArquivo);
+
+  imprimirClassFile(class_file);
+  
   return 0;
 }
