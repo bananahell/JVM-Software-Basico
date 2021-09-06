44b498822bfa8db19df688a56415c5525463858d
Pedro Nogueira <cmcg89034@gmail.com>
2021-09-05 23:25:02 -0300

    Refazendo o menu da main e adicionando outro .class para o helloworld

--- fonte/src/_main.cpp
+++ fonte/src/_main.cpp
@@ -12,21 +12,25 @@ int main(int argc, char *argv[]) {
   // Quando igual a 1 nao foi passado nenhum parametro
   if (argc > 1) {
     nome_classe = argv[1];
+  } else {
+    cout << "Using default.class...." << endl;
   }
 
   FILE *fp = fopen(nome_classe, "rb");
   if (fp == NULL) {
-    cout << "Erro no arquivo : " << nome_classe << endl;
+    cout << "There was a problem opening your file " << nome_classe << "...." << endl;
     return 0;
   }
 
   ClassFile *classe = new ClassFile(fp);
-  cout << "Magic number = " << classe->getMagicNumber() << endl;
-  cout << "Minor version = " << classe->getMinorVersion() << endl;
-  cout << "Major version = " << classe->getMajorVersion() << endl;
-  cout << "Interfaces count = " << classe->getInterfacesCount() << endl;
-  cout << "Fields count = " << classe->getFieldsCount() << endl;
-  cout << "Methods count = " << classe->getMethodsCount() << endl;
+  cout << "--------------------------------" << endl;
+  cout << "| Magic #        |  " << classe->getMagicNumber() << endl;
+  cout << "| Minor v.       |  " << classe->getMinorVersion() << endl;
+  cout << "| Major v.       |  " << classe->getMajorVersion() << endl;
+  cout << "| Interfaces #   |  " << classe->getInterfacesCount() << endl;
+  cout << "| Fields #       |  " << classe->getFieldsCount() << endl;
+  cout << "| Methods #      |  " << classe->getMethodsCount() << endl;
+  cout << "--------------------------------" << endl;
   fclose(fp);
   return 0;
 }
